eb29dfee313dc7088d211096953b9f20c9ff57ca
adr-sik <adrsik1997@gmail.com>
2026-01-28 17:09:01 +0100

    feat: added controllers and json configuration

---
+++ src/Catalog/Presentation/Controllers/ComponentsController.cs
@@ -0,0 +1,64 @@
+﻿using Catalog.Application.Interfaces;
+using Catalog.Domain.Models.ProductComponents;
+using Catalog.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ComponentsController : ControllerBase
+    {
+        private readonly IComponentsService _componentsService;
+
+        public ComponentsController(IComponentsService componentsService) =>
+            _componentsService = componentsService;
+
+        [HttpGet]
+        public async Task<List<BaseComponent>> Get() =>
+            await _componentsService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<BaseComponent>> Get(string id)
+        {
+            var component = await _componentsService.GetAsync(id);
+            if (component is null)
+            {
+                return NotFound();
+            }
+            return component;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(BaseComponent newComponent)
+        {
+            await _componentsService.CreateAsync(newComponent);
+            return CreatedAtAction(nameof(Get), new { id = newComponent.Id }, newComponent);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, BaseComponent updatedComponent)
+        {
+            var component = await _componentsService.GetAsync(id);
+            if (component is null)
+            {
+                return NotFound();
+            }
+            updatedComponent.Id = component.Id;
+            await _componentsService.UpdateAsync(id, updatedComponent);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var component = await _componentsService.GetAsync(id);
+            if (component is null)
+            {
+                return NotFound();
+            }
+            await _componentsService.RemoveAsync(id);
+            return NoContent();
+        }
+    }
+}
