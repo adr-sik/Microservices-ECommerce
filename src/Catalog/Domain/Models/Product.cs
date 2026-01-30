5f954df71afc58fc16986eda7496fcf6159ba9f1
adr-sik <adrsik1997@gmail.com>
2026-01-30 09:48:47 +0100

    fix: removed comments

--- src/Catalog/Domain/Models/Product.cs
+++ src/Catalog/Domain/Models/Product.cs
@@ -1,18 +1,10 @@
 ﻿namespace Catalog.Domain.Models
 {
-    //[JsonPolymorphic(TypeDiscriminatorPropertyName = "productType")]
-    //[JsonDerivedType(typeof(Laptop), typeDiscriminator: "laptop")]
-    //[JsonDerivedType(typeof(Phone), typeDiscriminator: "phone")]
-    //[BsonDiscriminator(RootClass = true)]
-    //[BsonKnownTypes(typeof(Laptop), typeof(Phone))]
     public abstract class Product
     {
-        //[BsonId]
-        //[BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         public required string Brand { get; set; }
         public required string Model { get; set; }
-        //[BsonRepresentation(BsonType.Decimal128)]
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string? Description { get; set; }
--- src/Catalog/Domain/Models/ProductComponents/Cpu.cs
+++ src/Catalog/Domain/Models/ProductComponents/Cpu.cs
@@ -5,7 +5,6 @@ namespace Catalog.Domain.Models.ProductComponents
 {
     public class Cpu<TBrand> : BaseComponent where TBrand : Enum
     {
-        //[BsonRepresentation(BsonType.String)]
         public required TBrand Brand { get; set; }
         public required string Model { get; set; }
         public int NumberOfCores { get; set; }
