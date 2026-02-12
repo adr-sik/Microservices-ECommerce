daa2bf7b3d39bc5c5821c77c38beda438047a173
adr-sik <adrsik1997@gmail.com>
2026-02-12 12:50:47 +0100

    feat: add value object for `Price`

---
+++ src/Catalog.Domain/ValueObjects/Price.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Domain.Exceptions;
+
+namespace Catalog.Domain.ValueObjects
+{
+    internal record Price
+    {
+        internal decimal Value { get; private set; }
+
+        internal Price(decimal value)
+        {
+            if (value < 0)
+                throw new DomainValidationException("Price cannot be negative.", nameof(value));
+
+            Value = Math.Round(value, 2);
+        }
+
+        public static implicit operator decimal(Price price) => price.Value;
+        public static implicit operator Price(decimal value) => new(value);
+    }
+}
