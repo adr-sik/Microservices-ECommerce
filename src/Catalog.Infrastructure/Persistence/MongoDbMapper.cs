7c5a41767e0cbd3e17bc1bccc7f4ff5a21d8c66a
adr-sik <adrsik1997@gmail.com>
2026-02-16 11:43:16 +0100

    feat: add Infrastructure layer

---
+++ src/Catalog.Infrastructure/Persistence/MongoDbMapper.cs
@@ -0,0 +1,61 @@
+﻿using Catalog.Domain.Entities;
+using Catalog.Domain.Entities.ProductComponents;
+using Catalog.Domain.Enums;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace Catalog.Infrastructure.Persistence
+{
+    public static class MongoDbMapper
+    {
+        private static bool _isMapped = false;
+
+        public static void MapClasses()
+        {
+            if (_isMapped) return;
+
+            BsonSerializer.RegisterSerializer(new DecimalSerializer(BsonType.Decimal128));
+
+            BsonSerializer.RegisterSerializer(new EnumSerializer<CpuBrand>(BsonType.String));
+            BsonSerializer.RegisterSerializer(new EnumSerializer<GpuBrand>(BsonType.String));
+            BsonSerializer.RegisterSerializer(new EnumSerializer<ProductType>(BsonType.String));
+
+            BsonClassMap.RegisterClassMap<Product>(cm =>
+            {
+                cm.AutoMap();
+                cm.IdMemberMap
+                    .SetSerializer(new StringSerializer(BsonType.ObjectId))
+                    .SetIdGenerator(StringObjectIdGenerator.Instance);
+                cm.SetIsRootClass(true);
+
+                MapDerivedClasses<Product>(cm);
+            });
+
+            BsonClassMap.RegisterClassMap<BaseComponent>(cm =>
+            {
+                cm.AutoMap();
+                cm.IdMemberMap
+                    .SetSerializer(new StringSerializer(BsonType.ObjectId))
+                    .SetIdGenerator(StringObjectIdGenerator.Instance);
+                cm.SetIsRootClass(true);
+
+                MapDerivedClasses<BaseComponent>(cm);
+            });
+
+            _isMapped = true;
+        }
+
+        private static void MapDerivedClasses<TBase>(BsonClassMap cm) where TBase : class
+        {
+            var derivedTypes = typeof(TBase).Assembly.GetTypes()
+                .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(TBase)));
+
+            foreach (var derivedType in derivedTypes)
+            {
+                cm.AddKnownType(derivedType);
+            }
+        }
+    }
+}
---
+++ src/Catalog.Infrastructure/Repositories/ProductsRepository.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Application.Interfaces;
+using Catalog.Domain.Entities;
+using Catalog.Infrastructure.Persistence;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ProductsRepository : IProductsRepository
+    {
+        private readonly MongoDbContext _context;
+
+        public ProductsRepository(MongoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyCollection<Product>> GetAsync() =>
+            await _context.Products.Find(_ => true).ToListAsync();
+
+        public async Task<Product?> GetAsync(string id) =>
+            await _context.Products.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Product newProduct) =>
+            await _context.Products.InsertOneAsync(newProduct);
+
+        public async Task UpdateAsync(string id, Product updatedProduct) =>
+            await _context.Products.ReplaceOneAsync(x => x.Id == id, updatedProduct);
+
+        public async Task RemoveAsync(string id) =>
+            await _context.Products.DeleteOneAsync(x => x.Id == id);
+
+    }
+}
