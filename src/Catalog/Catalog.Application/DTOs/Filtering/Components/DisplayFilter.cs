9c6f0f6d0edfafb3811e6eb99438382f306aa235
adr-sik <adrsik1997@gmail.com>
2026-03-06 13:40:45 +0100

    fix: replaced double for decimal for screen size

--- src/Catalog/Catalog.Application/DTOs/Filtering/Components/DisplayFilter.cs
+++ src/Catalog/Catalog.Application/DTOs/Filtering/Components/DisplayFilter.cs
@@ -5,8 +5,8 @@ namespace Catalog.Application.DTOs.Filtering.Components
     public record DisplayFilter : ComponentFilter
     {
         public ProductType? DesignedFor { get; set; }
-        public double? MinScreenSizeInches { get; set; }
-        public double? MaxScreenSizeInches { get; set; }
+        public decimal? MinScreenSizeInches { get; set; }
+        public decimal? MaxScreenSizeInches { get; set; }
         public string? Resolution { get; set; }
         public int? MinRefreshRateHz { get; set; }
         public int? MaxRefreshRateHz { get; set; }
